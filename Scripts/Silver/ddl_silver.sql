19b8242560f3c7138591d16d38cf00ef0a2112e8
M Ankitha <mankitha0606@gmail.com>
2026-03-09 22:56:29 +0530

    Create ddl_silver.sql

---
+++ Scripts/Silver/ddl_silver.sql
@@ -0,0 +1,69 @@
+IF OBJECT_ID('silver_crm_cust_info','U') IS NOT NULL
+   DROP TABLE silver_crm_cust_info;
+CREATE TABLE silver_crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(10),
+	cst_gndr NVARCHAR(10),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE() -- Metadata column
+);
+
+IF OBJECT_ID('silver_crm_prd_info','U') IS NOT NULL
+   DROP TABLE silver_crm_prd_info;
+CREATE TABLE silver_crm_prd_info(
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(10),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+IF OBJECT_ID('silver_crm_sale_details','U') IS NOT NULL
+   DROP TABLE silver_crm_sale_details;
+CREATE TABLE silver_crm_sale_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+IF OBJECT_ID('silver_erp_cust_az12','U') IS NOT NULL
+   DROP TABLE silver_erp_cust_az12;
+CREATE TABLE silver_erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(10),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+IF OBJECT_ID('silver_erp_loc_a101','U') IS NOT NULL
+   DROP TABLE silver_erp_loc_a101;
+CREATE TABLE silver_erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+IF OBJECT_ID('silver_erp_px_cat_g1v2','U') IS NOT NULL
+   DROP TABLE silver_erp_px_cat_g1v2;
+CREATE TABLE silver_erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(10),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
